b8a5b0b7429916e2577a83610e165a485308f4a7
RealSanjeev <45820805+realsanjeev@users.noreply.github.com>
2023-05-19 15:59:06 +0000

    Adding deployment web

---
+++ Web-app/app.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, render_template, request
+import music_recognizer
+
+# create an instance of Flask class
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template("index.html")
+
+@app.route("/upload", methods=["POST"])
+def recognize():
+    if request.method == 'POST':
+        print(f"[INFO] POST method")
+        try:
+            print(f"[INFO] try method")
+            music_file = request.files.get("file")
+            print(f"[INFO] try method:  {music_file}")
+        except Exception as err:
+            return render_template('sorry.html', message=f'Please provide a valid file: {err}')
+        
+        print("[INFO] up")
+        if music_file is None:
+            return render_template('sorry.html', message='Please provide a valid file.')
+        
+        save_path = os.path.join("static/music/", music_file.filename)
+        music_file.save(save_path)
+        
+        print(f"Filename: {music_file.filename}")
+        print(f"Saved as: {save_path}")
+        
+        # Perform music recognition here
+        music_mfcc = music_recognizer.get_mfcc(save_path)
+        print("-"*30)
+        if music_mfcc is None:
+          return render_template('sorry.html', message='Error loading in server')
+        print(music_mfcc.shape)
+        print("-"*30)
+        predictions = music_recognizer.prediction(music_mfcc)
+        path, result = music_recognizer.probability_graph_path(predictions)
+        
+        return render_template("index.html", filename=music_file.filename, result=result)
+    
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Web-app/music_recognizer.py
@@ -0,0 +1,71 @@
+import os
+import math 
+import librosa
+import numpy as np
+from matplotlib import pyplot as plt
+from tensorflow.keras.models import load_model
+
+
+SAMPLE_RATE = 22050
+SEGMENT_LENGTH = 6 #in sec
+MODEL_PATH = "./models/my_cnn_model_for_mfcc.h5"
+GENRES = ['blues', 'hiphop', 'rock', 'pop', 'disco', 'reggae', 'jazz', 'country', 'classical', 'metal']
+model = load_model("/workspaces/Music-gerne-classification-using-deep-learning/Web-app/models/my_cnn_model_for_mfcc.h5")
+
+def get_mfcc(music_path, parts=5, n_mfcc=13, n_fft=2048, hop_length=512):
+  # Divide given music in  6 sec segment
+  samples_per_segment = SEGMENT_LENGTH* 22050
+  mfcc_per_segment = math.ceil(samples_per_segment / hop_length)
+  try:
+    y, sr = librosa.load(music_path, sr=SAMPLE_RATE)
+    durations = librosa.get_duration(y=y, sr=sr)
+    parts = int(durations/ SEGMENT_LENGTH)
+  except:
+    print(f"[INFO] Please check given music path: {music_path}.")
+    return None
+  music_all_mfcc = []
+  for part in range(parts):
+  # find the start and finish of segment
+    start = samples_per_segment * part
+    end = start + samples_per_segment
+
+    # extract mfcc
+    mfcc = librosa.feature.mfcc(y=y[start:end], sr=sr, n_mfcc=n_mfcc, n_fft=n_fft, hop_length=hop_length)
+    mfcc = mfcc.T
+
+    if len(mfcc) == mfcc_per_segment:
+      # resize to fit in cnn model
+      mfcc = np.resize(mfcc, (mfcc.shape[0], mfcc.shape[1], 1))
+      music_all_mfcc.append(mfcc)
+    else:
+      print(f"[INFO] {part} segment of {file} is not included ")
+  return np.array(music_all_mfcc)
+  
+
+def prediction(test_mfcc):
+  prediction = model.predict(test_mfcc)
+  return prediction
+
+def probability_graph_path(prediction):
+  mean_pred = np.mean(prediction, axis=0)
+  plt.figure(figsize=(10, 5))
+  plt.bar(GENRES, height=mean_pred*100)
+
+  # Plot for percentage of confidence
+  plt.title("Prediction Confidence")
+  plt.xlabel("Labels")
+  plt.ylabel("Perrcentage of confidence")
+  plt.ylim(0,100)
+  path = "probability_graph.png"
+  if os.path.exists(f"static/{path}"):
+    os.remove(f"static/{path}")
+    print(f"File 'static/{path}' deleted successfully.")
+  else:
+    print(f"File 'static/{path}' does not exist.")
+  plt.savefig(f"static/{path}", facecolor='y', bbox_inches="tight",
+            pad_inches=0.3, transparent=True)
+  
+  print(F"[INFO] lABEL: {np.argmax(mean_pred)}")
+  genre_result = GENRES[np.argmax(mean_pred)]
+
+  return path, genre_result
